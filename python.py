9ae2ffee7eaf06cd9392f49b9c1fce1105445011
alfghanhoound09 <mquoc4592@gmail.com>
2026-04-01 23:59:20 +0700

    feat: add bloom

---
+++ python.py
@@ -0,0 +1,10 @@
+user =  input ("im blooming")
+bloom = " "
+while True:
+    for i in range(len(bloom)):
+        if user == "bloom":
+            bloom += "bloomed bloomed"
+    
+
+    print(bloom)
+exit()
